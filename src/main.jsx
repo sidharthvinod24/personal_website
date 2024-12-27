3cba236f109b33f18386d4c3031f6eb17af8b124
sidharthvinod24 <sidharthvinod24@gmail.com>
2024-12-28 01:24:16 +0800

    Added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { inject } from "@vercel/analytics";
 
-createRoot(document.getElementById('root')).render(
+inject();
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
