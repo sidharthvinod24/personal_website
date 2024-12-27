6666235f1327b13118246741f5be2bbdd30faf98
sidharthvinod24 <sidharthvinod24@gmail.com>
2024-12-28 01:01:42 +0800

    Created my personal website

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
           whenever I can
         </p>
         <a
-          href="mailto:"
+          href="mailto:sidvinpersonalwebsite@gmail.com"
           className="text-nowrap rounded-lg border border-amber-600 bg-black px-5 py-3 text-lg font-bold text-white shadow-lg shadow-amber-700 transition-all duration-300 hover:-translate-y-2 hover:shadow-xl hover:shadow-amber-600"
         >
           Contact Me
